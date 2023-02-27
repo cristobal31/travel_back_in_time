e7c4fe312e5c84d6b6e6bfddf959e3c1e7e92ae1
Akram Hassan <akramhassan1502@gmail.com>
2023-02-27 05:23:09 +0100

    new/create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,28 @@
 class EventsController < ApplicationController
+  before_action :set_event
+  def index
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(list_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def list_params
+    params.require(:event).permit(:name, :photo)
+  end
 end
