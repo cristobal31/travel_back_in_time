fb5bd14901991af073e7bdcd9b0ade96b1f4e61e
Daniel Pippin <danielpippin986@gmail.com>
2023-02-28 19:00:08 +0100

    needs more work

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,25 @@ class BookingsController < ApplicationController
   # def destroy
   #   @bookings = Bookings.find(params[:id])
   #   @bookings.destroy
-  # end
+  def new
+    @bookings = Booking.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @booking = bookings.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    param.require(:booking).permit(submit)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  delete "bookmarks/:id", to: "bookmarks#destroy", as: :delete
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end
