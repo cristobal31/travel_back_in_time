798a87fe7a4703c2f62c6098cb3c0f062d196767
cristobal fernandez <cristobal.sch@gmail.com>
2023-03-01 15:01:55 +0100

    seedrefact

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,69 +13,13 @@ User.destroy_all
 daniel = User.create!(email: "danielpippino986@gmail.com", password: "123456", first_name: "Daniel", last_name: "Pippin")
 luca = User.create!(email: "lucaiscool@gmail.com", password: "12345678", first_name: "Luca", last_name: "Bonaparte")
 
-events = Event.create!([{
-  user_id: User.all.sample.id,
-  category: "politics",
-  era: "late modern",
-  name: "fall of berlin wall",
-  year: "1989",
-  price: 100,
-  description: "fall of berlin wall"
-},
-
-{
-  user_id: User.all.sample.id,
-  category: "nature",
-  era: "prehistory",
-  name: "trex hunt",
-  year: "2000000 bc",
-  price: 200,
-  description: "Watch a Trex hunting",
-},
 
 
-{
-  user_id: User.all.sample.id,
-  category: "architecture",
-  era: "ancient",
-  name: "creation on the pyramids of gizah",
-  year: "2250 bc",
-  price: 150,
-  description: "watch the pyramids of gizah being built",
-},
 
 
-{
-  user_id: User.all.sample.id,
-  category: "science",
-  era: "late modern",
-  name: "moonlanding",
-  year: "1969",
-  price: 300,
-  description: "join the first moonlanding",
-},
-
-{
-  user_id: User.all.sample.id,
-  category: "people",
-  era: "late modern",
-  name: "woodstock",
-  year: "1967",
-  price: 169,
-  description: "Be part of the summer of love",
-},
-
-
-{
-  user_id: User.all.sample.id,
-  category: "science",
-  era: "late modern",
-  name: "a lecture with albert einstein",
-  year: "1938",
-  price: 400,
-  description: "Spend a day with the smartest human of all time"
-}])
-
-
+berlin_wall = Event.create!(user_id: User.all.sample.id, category: "politics", era: "late modern", name: "fall of berlin wall", year: "1989", price: 100, description: "fall of berlin wall")
+file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/berlin_wall_mrdcib.jpg")
+berlin_wall.photo.attach(io: file, filename: "berlin wall.jpeg", content_type: "image/jpeg")
+berlin_wall.save
 
 puts "no"
