5de45aaffeaabc0a1b5797f496c5b84f1f60b99d
Akram Hassan <akramhassan1502@gmail.com>
2023-03-02 16:00:32 +0100

    finished creating an event and button hovering

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,6 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
-
   def new
     @event = Event.new
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :category, inclusion: %w"[ people music science politics war nature architecture ]"
-  validates :era, inclusion: ["prehistory", "ancient", "post-classical", "early modern", "late modern"]
+  validates :era, inclusion: ["prehistory", "ancient", "post-classical", "early-modern", "late-modern"]
   validates :name, :era, :category, presence: true
   validates :name, uniqueness: true
 end
