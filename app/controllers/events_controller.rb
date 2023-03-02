834f0fb28618289c6771a3e20eac6b15d9160f3e
Daniel Pippin <danielpippin986@gmail.com>
2023-03-02 16:36:15 +0100

    finished search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,7 +28,6 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     if @event.save
-      raise
       redirect_to events_path(@event)
     else
       render :new, status: :unprocessable_entity
