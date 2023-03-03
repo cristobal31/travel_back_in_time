7e320c0d0bf1df1b6cd9ca1e36cb9eb3d474a84f
cristobal fernandez <cristobal.sch@gmail.com>
2023-03-03 12:24:35 +0100

    seed description

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,32 +13,32 @@ User.destroy_all
 daniel = User.create!(email: "danielpippin986@gmail.com", password: "123456", first_name: "Daniel", last_name: "Pippin")
 luca = User.create!(email: "lucaiscool@gmail.com", password: "12345678", first_name: "Luca", last_name: "Bonaparte")
 
-berlin_wall = Event.create!(user_id: User.all.sample.id, category: "politics", era: "late modern", name: "fall of berlin wall", year: "1989", price: 100, description: "fall of berlin wall")
+berlin_wall = Event.create!(user_id: User.all.sample.id, category: "politics", era: "late modern", name: "fall of Berlin wall", year: "1989", price: 100, description: "The fall of the Berlin Wall on November 9, 1989, marked the end of the Cold War and the reunification of Germany.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/berlin_wall_mrdcib.jpg")
 berlin_wall.photo.attach(io: file, filename: "berlin wall.jpeg", content_type: "image/jpeg")
 berlin_wall.save
 
-moon_landing = Event.create!(user_id: User.all.sample.id, category: "science", era: "late modern", name: "moon landing", year: "1969", price: 300, description: "join the first moonlanding")
+moon_landing = Event.create!(user_id: User.all.sample.id, category: "science", era: "late modern", name: "moon Landing", year: "1969", price: 300, description: "On July 20, 1969, Neil Armstrong and Buzz Aldrin became the first humans to land on the moon.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/moon_landing_yp7avm.jpg")
 moon_landing.photo.attach(io: file, filename: "moon landing.jpeg", content_type: "image/jpeg")
 moon_landing.save
 
-giza = Event.create!(user_id: User.all.sample.id, category: "architecture", era: "ancient", name: "creation on the pyramids of giza", year: "2250 bc", price: 150, description: "watch the pyramids of giza being built")
+giza = Event.create!(user_id: User.all.sample.id, category: "architecture", era: "ancient", name: "The Pyramids of Giza", year: "2250 bc", price: 150, description: "The Pyramid of Giza in Egypt, one of the ancient Seven Wonders of the World, was built over 20 years by thousands of laborers using limestone blocks.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678044/timescape/piramit_of_giza_pmih4c.jpg")
 giza.photo.attach(io: file, filename: "giza.jpeg", content_type: "image/jpeg")
 giza.save
 
-woodstock = Event.create!(user_id: User.all.sample.id, category: "people", era: "late modern", name: "woodstock", year: "1967", price: 169, description: "Be part of the summer of love")
+woodstock = Event.create!(user_id: User.all.sample.id, category: "people", era: "late modern", name: "woodstock", year: "1967", price: 420, description: "Woodstock was a three-day music festival in August 1969 that attracted over 400,000 people and became a defining moment of the counterculture movement.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/woodstock_fmyck1.jpg")
 woodstock.photo.attach(io: file, filename: "woodstock.jpeg", content_type: "image/jpeg")
 woodstock.save
 
-einstein = Event.create!(user_id: User.all.sample.id, category: "science", era: "late modern", name: "a lecture with albert einstein", year: "1938", price: 400, description: "spend a day with the smartest human of all time")
+einstein = Event.create!(user_id: User.all.sample.id, category: "science", era: "late modern", name: "a lecture with Albert Einstein", year: "1938", price: 400, description: "Albert Einstein's lectures were known for their clarity and simplicity, and he spoke on topics ranging from relativity to philosophy.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/einstein_lecture_dz3ljp.jpg")
 einstein.photo.attach(io: file, filename: "einstein .jpeg", content_type: "image/jpeg")
 einstein.save
 
-trex_hunt = Event.create!(user_id: User.all.sample.id, category: "nature", era: "prehistory", name: "trex hunt", year: "2000000 bc", price: 200, description: "Watch a Trex hunting")
+trex_hunt = Event.create!(user_id: User.all.sample.id, category: "nature", era: "prehistory", name: "Pet a T-Rex", year: "65000000 bc", price: 200, description: "A formidable and dangerous dinosaur, but they also love to be petted.")
 file = URI.open("https://res.cloudinary.com/dxrk5d6qu/image/upload/v1677678043/timescape/t-rex_hunt_b7tjat.jpg")
 trex_hunt.photo.attach(io: file, filename: "trex_hunt .jpeg", content_type: "image/jpeg")
 trex_hunt.save
